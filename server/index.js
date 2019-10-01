f497505725ab3fd037fa10ecb1ef223db30b67e4
cris93g <37388115+cris93g@users.noreply.github.com>
2019-10-01 07:18:46 -0500

    started on addME

---
+++ server/index.js
@@ -0,0 +1,78 @@
+require("dotenv").config();
+const express = require("express");
+const massive = require("massive");
+const cors = require("cors");
+const { json } = require("body-parser");
+const passport = require("passport");
+const session = require("express-session");
+const app = express();
+const port = process.env.PORT || 3001;
+const {
+  getUser,
+  strat,
+  logout
+} = require(`${__dirname}/controllers/authCtrl/authCtrl`);
+
+app.use(cors());
+app.use(json());
+
+massive(process.env.CONNECTION_STRING).then(dbinstance => {
+  app.set("db", dbinstance);
+});
+
+//brings sessions
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+//functions for passport
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(strat);
+
+// What properties do we want our user to have on session?
+passport.serializeUser((profile, done) => {
+  console.log(profile);
+  const db = app.get("db");
+  db.getUserByAuthid(profile.id).then(user => {
+    // console.log(profile.name);
+    if (!user[0]) {
+      db.addUserByAuthid(profile.id, profile.picture, profile.displayName)
+        .then(response => {
+          return done(null, response[0]);
+        })
+        .catch(err => console.log(err));
+    } else {
+      return done(null, user[0]);
+    }
+  });
+});
+
+// Put user on req.session
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+app.get(
+  "/login",
+  passport.authenticate("auth0", {
+    successRedirect: "http://localhost:3000/",
+    failureRedirect: "http://localhost:3000/#/"
+  })
+);
+
+app.use((req, res, next) => {
+  if (!req.session.cart) req.session.cart = [];
+  next();
+});
+
+app.get("/me", getUser);
+app.get("/logout", logout);
+
+app.listen(port, () => {
+  console.log(`app is listening on port ${port}`);
+});
---
+++ server/routes/routes.js
@@ -0,0 +1,6 @@
+const {} = require("../controllers/postControllers/postController");
+const {} = require("../controllers/userControllers/UserController");
+
+module.exports = app => {
+  app.get();
+};
